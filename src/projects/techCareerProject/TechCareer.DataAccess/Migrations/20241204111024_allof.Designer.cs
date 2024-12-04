76f364b5f7b51f967364279ad9b7d0419c25cf6e
ismailylmz31 <64439554+ismailylmz31@users.noreply.github.com>
2024-12-04 14:17:56 +0300

    migration deleted and updated

--- src/projects/techCareerProject/TechCareer.DataAccess/Migrations/20241204111024_allof.Designer.cs
+++ src/projects/techCareerProject/TechCareer.DataAccess/Migrations/20241204111024_allof.Designer.cs
@@ -12,8 +12,8 @@ using TechCareer.DataAccess.Contexts;
 namespace TechCareer.DataAccess.Migrations
 {
     [DbContext(typeof(BaseDbContext))]
-    [Migration("20241204104817_FixCategoryEventNavigation")]
-    partial class FixCategoryEventNavigation
+    [Migration("20241204111024_allof")]
+    partial class allof
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -88,7 +88,7 @@ namespace TechCareer.DataAccess.Migrations
                         new
                         {
                             Id = 1,
-                            CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            CreatedDate = new DateTime(2024, 12, 4, 11, 10, 23, 618, DateTimeKind.Utc).AddTicks(4329),
                             Name = "Admin"
                         });
                 });
@@ -153,12 +153,12 @@ namespace TechCareer.DataAccess.Migrations
                         new
                         {
                             Id = 1,
-                            CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            CreatedDate = new DateTime(2024, 12, 4, 11, 10, 23, 620, DateTimeKind.Utc).AddTicks(757),
                             Email = "admin@admin.com",
                             FirstName = "Admin",
                             LastName = "Techcareer",
-                            PasswordHash = new byte[] { 104, 39, 1, 103, 108, 134, 144, 105, 184, 131, 92, 159, 75, 102, 83, 4, 61, 200, 142, 45, 164, 161, 112, 87, 115, 246, 211, 100, 134, 233, 147, 247, 69, 125, 144, 28, 73, 152, 62, 8, 179, 255, 180, 50, 110, 167, 187, 141, 18, 15, 211, 245, 249, 163, 200, 114, 152, 79, 131, 172, 193, 59, 41, 226 },
-                            PasswordSalt = new byte[] { 228, 209, 180, 119, 52, 164, 117, 121, 143, 127, 62, 56, 149, 108, 133, 206, 238, 30, 233, 164, 87, 174, 212, 127, 78, 95, 199, 67, 202, 175, 71, 49, 82, 55, 213, 178, 158, 116, 95, 202, 248, 214, 146, 211, 95, 61, 28, 46, 30, 120, 125, 18, 115, 188, 63, 236, 43, 187, 118, 46, 142, 22, 131, 171, 48, 83, 83, 64, 54, 137, 216, 211, 197, 238, 217, 63, 160, 229, 253, 118, 161, 235, 134, 157, 74, 231, 11, 109, 43, 148, 65, 68, 16, 142, 100, 208, 113, 39, 185, 222, 201, 190, 236, 192, 7, 143, 234, 172, 11, 251, 79, 103, 89, 120, 83, 125, 80, 249, 123, 1, 89, 137, 187, 0, 162, 67, 107, 238 },
+                            PasswordHash = new byte[] { 140, 30, 3, 35, 165, 63, 242, 66, 60, 158, 173, 227, 224, 180, 46, 55, 239, 240, 26, 241, 195, 254, 142, 22, 232, 28, 79, 222, 150, 188, 198, 61, 164, 179, 133, 135, 21, 140, 149, 77, 231, 212, 233, 200, 248, 106, 48, 20, 18, 43, 15, 61, 74, 211, 155, 176, 167, 161, 177, 3, 233, 14, 17, 170 },
+                            PasswordSalt = new byte[] { 146, 255, 15, 226, 72, 33, 67, 18, 203, 58, 127, 227, 104, 127, 55, 58, 246, 253, 44, 213, 128, 87, 98, 195, 64, 56, 50, 215, 176, 245, 84, 46, 89, 234, 251, 179, 113, 208, 40, 132, 4, 129, 205, 188, 92, 136, 251, 94, 174, 251, 71, 96, 68, 50, 240, 224, 92, 237, 185, 211, 250, 119, 205, 77, 181, 108, 55, 54, 12, 211, 145, 189, 4, 152, 82, 181, 115, 138, 164, 145, 98, 148, 165, 221, 124, 103, 7, 155, 150, 98, 152, 20, 238, 120, 177, 121, 33, 101, 28, 232, 32, 146, 212, 91, 56, 193, 100, 195, 91, 74, 57, 89, 144, 129, 195, 210, 33, 82, 249, 244, 236, 198, 164, 107, 207, 155, 188, 194 },
                             Status = true
                         });
                 });
@@ -204,7 +204,7 @@ namespace TechCareer.DataAccess.Migrations
                         new
                         {
                             Id = 1,
-                            CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            CreatedDate = new DateTime(2024, 12, 4, 11, 10, 23, 620, DateTimeKind.Utc).AddTicks(5040),
                             OperationClaimId = 1,
                             UserId = 1
                         });
@@ -315,10 +315,8 @@ namespace TechCareer.DataAccess.Migrations
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
                     b.Property<DateTime>("CreatedDate")
-                        .ValueGeneratedOnAdd()
                         .HasColumnType("datetime2")
-                        .HasColumnName("CreatedDate")
-                        .HasDefaultValueSql("GETDATE()");
+                        .HasColumnName("CreatedDate");
 
                     b.Property<DateTime?>("DeletedDate")
                         .HasColumnType("datetime2");
@@ -337,7 +335,7 @@ namespace TechCareer.DataAccess.Migrations
 
                     b.Property<Guid>("InstructorId")
                         .HasColumnType("uniqueidentifier")
-                        .HasColumnName("InstrutorId");
+                        .HasColumnName("InstructorId");
 
                     b.Property<bool>("IsCertified")
                         .HasColumnType("bit")
